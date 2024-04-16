82a762eb19c17f45110ae8808b8fc50f673dce83
deanofwalls <deanofwalls@gmail.com>
2024-04-16 14:20:56 -0400

    saving progress

--- src/main/java/com/github/curriculeon/NumberUtilities.java
+++ src/main/java/com/github/curriculeon/NumberUtilities.java
@@ -3,17 +3,43 @@ package com.github.curriculeon;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        StringBuilder answers = new StringBuilder();
+        for (int i = start; i < stop; i++) {
+            if(i % 2 == 0){
+                answers.append(i);
+            }
+        }
+        return answers.toString();
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        StringBuilder answers = new StringBuilder();
+        for (int i = start; i < stop; i++) {
+            if(i % 2 != 0){
+                answers.append(i);
+            }
+        }
+        return answers.toString();
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        //to find if a number is square you must first get sqrt of number
+        //if the sqrt is an int, the number is square
+        System.out.println("start: " + start + ", stop: " + stop + ", step: " + step);
+        StringBuilder answers = new StringBuilder();
+        for (int i = start; i <= stop; i+= step) {
+            double sqrt = Math.sqrt(i);
+            double floor = Math.floor(sqrt);
+            System.out.println("The square root of " + i + ": " + sqrt + ", Floor: " + floor);
+            if(sqrt == Math.floor(sqrt)){
+                System.out.println(i + " is a perfect square");
+                System.out.println(i + " was appended");
+                answers.append(i);
+            }
+        }
+        return answers.toString();
     }
 
     public static String getRange(int start) {
